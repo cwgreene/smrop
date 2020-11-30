52061ed11be72cdad6bc193b4db8f9d8fde4ea59
Chris Greene <archgoon@gmail.com>
2020-11-29 16:31:54 -0800

    Smrop!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smrop',
+    version='0.1',
+    author="Chris Greene",
+    author_email="archgoon@gmail.com",
+    license="MIT",
+    packages=['smrop'],
+)
